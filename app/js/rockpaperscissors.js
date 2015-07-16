a8457392ba56394b29a8be3ce9df68d185ba9f9a
chamberlainguy <guy.821020312562@gmail.com>
2015-07-16 18:30:12 +1000

    Unit 5 Homework. Added logic to finish project

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,19 +41,19 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-		if (playerMove == "rock" && computerMove == "paper") {
+		if (playerMove === "rock" && computerMove === "paper") {
 			winner = "computer"; }
-		else if (playerMove == "rock" && computerMove == "scissors") {
+		else if (playerMove === "rock" && computerMove === "scissors") {
 			winner = "player"; }
-		else if (playerMove == "paper" && computerMove == "rock") {
+		else if (playerMove === "paper" && computerMove === "rock") {
 			winner = "player"; }
-		else if (playerMove == "paper" && computerMove == "scissors") {
+		else if (playerMove === "paper" && computerMove === "scissors") {
 			winner = "computer"; }
-		else if (playerMove == "scissors" && computerMove == "rock") {
+		else if (playerMove === "scissors" && computerMove === "rock") {
 			winner = "computer"; }
-		else if (playerMove == "scissors" && computerMove == "paper") {
+		else if (playerMove === "scissors" && computerMove === "paper") {
 			winner = "player"; }
-		else if (playerMove == computerMove) {
+		else if (playerMove === computerMove) {
 		  winner = "tie"; }
 		else {
 			winner = "ERROR: Can't determine the result"; }
@@ -65,8 +65,25 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var Winner, computerMove;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    
+	  while (playerWins < 5 && computerWins < 5) {   
+	   	computerMove = getComputerMove();
+    	Winner = getWinner(getPlayerMove(),computerMove);
+    	console.log("Computer plays " + computerMove );
+    	console.log("Winner is " + Winner);
+			switch (Winner) {
+				case "player" : playerWins++;break;
+				case "computer" : computerWins++;
+			}    	
+    	console.log("Player has won " + playerWins + " times : Computer has won " + computerWins + " times");
+  	}	
+    
     return [playerWins, computerWins];
 }
+
+playToFive();
+
 
